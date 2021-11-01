6342626506492325fd9bb39bca729c790aeffa49
aboxofsox <idkwthfml@gmail.com>
2021-11-01 03:01:45 -0500

    Add project files.

---
+++ TaskbarTool/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskbarTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PositionBox.Items.Add("-- Select Position --");
+            PositionBox.SelectedIndex = 0;
+
+
+            PositionBox.Items.Add("Top");
+            PositionBox.Items.Add("Left");
+            PositionBox.Items.Add("Right");
+            PositionBox.Items.Add("Bottom");
+
+        }
+
+        private void SetTaskBar_Click(object sender, RoutedEventArgs e)
+        {
+            string Pos = "";
+
+            switch (PositionBox.Text.ToLower())
+            {
+                case "top":
+                    Pos = "01";
+                    break;
+                case "left":
+                    Pos = "00";
+                    break;
+                case "right":
+                    Pos = "02";
+                    break;
+                case "bottom":
+                    Pos = "03";
+                    break;
+                default:
+                    Pos = "03";
+                    break;
+            }
+
+            string message = "Moved taskbar to " + Pos;
+
+            RegKeyHandler.SetKeys(Pos, true);
+        }
+    }
+}
---
+++ TaskbarTool/RegKeys.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace TaskbarTool
+{
+    public class RegKeys
+    {
+    }
+
+    public class RegKeyHandler
+    { 
+        public static void SetKeys(string pos, bool restartExplorer)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3");
+
+            if (key != null)
+            {
+                byte[] data = (byte[])key.GetValue("Settings");
+                string originalHex = BitConverter.ToString(data);
+                string[] originalHexArray = originalHex.Split("-");
+
+                string[] value = "30,00,00,00,fe,ff,ff,ff,7a,f4,00,00,03,00,00,00,30,00,00,00,30,00,00,00,00,00,00,00,00,00,00,00,00,0a,00,00,30,00,00,00,60,00,00,00,01,00,00,00".Split(",");
+                string[] hex = new string[value.Length];
+                byte[] newHex = new byte[value.Length];
+
+                value[12] = pos;
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    string h = "0x" + value[i];
+                    hex[i] = h;
+                }
+
+                for (int i = 0; i < newHex.Length; i++)
+                {
+                    newHex[i] = Convert.ToByte(hex[i], 16);
+                }
+
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3", "Settings", newHex);
+
+                var explorer = Process.GetProcessesByName("explorer")[0];
+
+                if (restartExplorer == true)
+                {
+                    explorer.Kill();
+                }
+            }
+        }
+    }
+
+
+}
