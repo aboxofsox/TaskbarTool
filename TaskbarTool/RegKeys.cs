202c9c03c790ea04a3cebadda19cac33627e0501
aboxofsox <idkwthfml@gmail.com>
2021-11-01 22:20:39 -0500

    removed unncessary for loop

--- TaskbarTool/RegKeys.cs
+++ TaskbarTool/RegKeys.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Management;
 using Microsoft.Win32;
 using System.Diagnostics;
-using System.Windows.Controls;
 
 
 namespace TaskbarTool
 {
+    // Needs refactoring
     public class MultiDisplays
     {
         public static void Set(byte[] bytes)
@@ -31,9 +26,6 @@ namespace TaskbarTool
             {
                 throw new Exception("Registry key could not be found.");
             }
-
-
-
         }
     }
     
@@ -45,6 +37,7 @@ namespace TaskbarTool
         {
             RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3");
 
+            // Needs refactoring
             if (key != null)
             {
                 byte[] data = (byte[])key.GetValue("Settings");
@@ -52,20 +45,13 @@ namespace TaskbarTool
                 string[] originalHexArray = originalHex.Split("-");
 
                 string[] value = "30,00,00,00,fe,ff,ff,ff,7a,f4,00,00,03,00,00,00,30,00,00,00,30,00,00,00,00,00,00,00,00,00,00,00,00,0a,00,00,30,00,00,00,60,00,00,00,01,00,00,00".Split(",");
-                string[] hex = new string[value.Length];
                 byte[] newHex = new byte[value.Length];
 
                 value[12] = pos;
 
-                for (int i = 0; i < value.Length; i++)
-                {
-                    string h = "0x" + value[i];
-                    hex[i] = h;
-                }
-
                 for (int i = 0; i < newHex.Length; i++)
                 {
-                    newHex[i] = Convert.ToByte(hex[i], 16);
+                    newHex[i] = Convert.ToByte(value[i], 16);
                 }
 
                 Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3", "Settings", newHex);
@@ -150,6 +136,4 @@ namespace TaskbarTool
         }
 
     }
-
-
 }
